45796a146630fc33aef98b7bc8e8fc8841b4f3f3
Bostnis <46979515+cr8tpro@users.noreply.github.com>
2019-09-17 10:56:42 +0200

    test styling

--- src/components/TopToolbar.vue
+++ src/components/TopToolbar.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-
-    <v-navigation-drawer app fixed v-model="showMenu">
+    <v-navigation-drawer app fixed v-model="showMenu" class="topMargin">
       <v-list dense>
-        <v-list-tile @click="">
+        <v-list-tile @click>
           <v-list-tile-action>
             <v-icon>settings</v-icon>
           </v-list-tile-action>
@@ -11,7 +10,7 @@
             <v-list-tile-title>Settings</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile @click="">
+        <v-list-tile @click>
           <v-list-tile-action>
             <v-icon>help</v-icon>
           </v-list-tile-action>
@@ -22,30 +21,31 @@
       </v-list>
     </v-navigation-drawer>
 
-
     <v-toolbar fixed app dark color="primary">
-
       <v-toolbar-side-icon @click.stop="toggleMenu"></v-toolbar-side-icon>
 
       <v-toolbar-title class="white--text">{{ title }}</v-toolbar-title>
-
     </v-toolbar>
-
   </div>
 </template>
 
+<style>
+.topMargin {
+  padding-top: 15px;
+}
+</style>
+
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
-  import { Getter } from 'vuex-class';
+import { Component, Vue } from 'vue-property-decorator';
+import { Getter } from 'vuex-class';
 
-  @Component
-  export default class TopToolbar extends Vue {
+@Component
+export default class TopToolbar extends Vue {
+  @Getter('title', { namespace: 'topToolbar' }) title!: string;
+  showMenu: boolean = false;
 
-    @Getter('title', {namespace: 'topToolbar'}) title!: string;
-    showMenu: boolean = false;
-
-    toggleMenu(): void {
-      this.showMenu = !this.showMenu;
-    }
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
   }
+}
 </script>
